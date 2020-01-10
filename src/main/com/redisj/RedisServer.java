fe6750d57c572e8120cb3715b18159486d568141
Artur Pogoda de la Vega <a.pogoda@venista.com>
2020-01-10 16:30:27 +0100

    Rempoved unused import.

--- src/main/com/redisj/RedisServer.java
+++ src/main/com/redisj/RedisServer.java
@@ -32,8 +32,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 
-import com.redisj.RedisServer.Worker;
-
 /**
  *
  * @author apdlv72
